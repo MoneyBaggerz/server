d78d0121ecc76428c3ca6257a7452bd400c431ac
Nevsimao <coday.ink@gmail.com>
2021-05-17 18:48:34 -0400

    added mongoose URI

---
+++ server/db/connections.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const mongooseURI = 'mongodb+srv://admin-simi:123simiAD@cluster0.spcgq.mongodb.net/MoneyBaggerzDatabase?retryWrites=true&w=majority'
+
+mongoose
+    .connect(mongooseURI, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+    })
+    .then((instance) => {
+        console.log(`connected to db: ${instance.connections[0].name}`)
+    })
+    .catch((error) => console.log('Connection Failed', error))
+
+module.exports = mongoose
