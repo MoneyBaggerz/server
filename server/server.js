6884851abecadbfcabc039940a82c140f38fe0dc
Nevsimao <coday.ink@gmail.com>
2021-05-17 18:42:51 -0400

    updated the server file

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express =  require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+const app = express()
+
+app.use(
+    cors()
+)
+app.use(
+    express.json()
+)
+app.use(
+	express.urlencoded({
+		extended: true,
+	})
+)
+
+// Routes
+app.get('/', (req, res) => { res.send('On!') })
+
+const shoes = require('./controllers/shoes')
+app.use('/shoes', shoes)
+
+dotenv.config()
+const port = ('port', process.env.PORT || 8000)
+
+// Start Server
+app.listen(port)
