2cbdc220e27823357c78e5c3eee00561cc925afc
Nevsimao <coday.ink@gmail.com>
2021-05-19 19:18:44 -0400

    added relation to brands and reviews, changed color and sive to hold an array

--- server/models/apparels.js
+++ server/models/apparels.js
@@ -1,18 +1,20 @@
 const mongoose = require('../db/connections')
+const reviews = require('./apparels_reviews')
 
 const apparelsSchema = new mongoose.Schema({
 	name: String,
 	price: Number,
-	size: Number,
-	color: String,
+	size: [Number],
+	color: [String],
 	availability: Boolean,
+	reviews: [reviews],
 	image: {
 		data: Buffer,
 		contentType: String
 	},
-	// brand: {
-	// 	type: mongoose.Schema.Types.ObjectId,ref:'Brands'
-	// }
+	brand: {
+		type: mongoose.Schema.Types.ObjectId, ref:'Brands'
+	}
 })
 
 module.exports = new mongoose.model('Apparels', apparelsSchema)
