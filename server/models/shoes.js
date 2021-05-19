56a83bb29b87880c6c88626e36d81526317eab59
Nevsimao <coday.ink@gmail.com>
2021-05-18 22:58:43 -0400

    updated schema filds

--- server/models/shoes.js
+++ server/models/shoes.js
@@ -2,7 +2,17 @@ const mongoose = require('../db/connections')
 
 const shoesSchema = new mongoose.Schema({
 	name: String,
-	price: Number
+	price: Number,
+	size: Number,
+	color: String,
+	availability: Boolean,
+	// brand: {
+	// 	type: mongoose.Schema.Types.ObjectId,ref:'Brands'
+	// },
+	image: {
+		data: Buffer,
+		contentType: String
+	}
 })
 
 module.exports = new mongoose.model('Shoes', shoesSchema)
