7dff7b8827067de0ec80942fdf2c80bed42f42b4
Nevsimao <coday.ink@gmail.com>
2021-05-18 23:05:43 -0400

    added new routes

--- server/controllers/shoes.js
+++ server/controllers/shoes.js
@@ -1,11 +1,39 @@
 const express = require('express')
-const shoes = require('../models/shoes')
+const Shoes = require('../models/shoes')
 const router = express.Router()
 
-// Get all shoes
+// Get shoes
 router.get('/', (req, res, next) => {
-	shoes.find({})
-		.then((entry) => res.json(entry))
+	Shoes.find({})
+		.then((shoes) => res.json(shoes))
+		.catch(next)
+})
+
+// GET a shoe
+router.get('/:id', (req, res, next) => {
+	Shoes.findById({ _id: req.params.id })
+		.then((shoes) => res.json(shoes))
+		.catch(next)
+})
+
+// POST a shoe
+router.post('/', (req, res, next) => {
+	Shoes.create(req.body)
+		.then((shoes) => res.json(shoes))
+		.catch(next)
+})
+
+// PUT a shoe
+router.put('/:id', (req, res, next) => {
+	Shoes.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
+		.then((shoes) => res.json(shoes))
+		.catch(next)
+})
+
+// DELETE a shoe
+router.delete('/:id', (req, res, next) => {
+	Shoes.findByIdAndDelete(req.params.id)
+		.then((shoes) => res.json(shoes))
 		.catch(next)
 })
 
