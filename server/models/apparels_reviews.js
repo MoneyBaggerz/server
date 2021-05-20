ad0b5fb98bf2b9fa1455b45d819991d6643f6959
Nevsimao <coday.ink@gmail.com>
2021-05-19 20:01:42 -0400

    made most schema fiels requiered and added relations to user and apparels

--- server/models/apparels_reviews.js
+++ server/models/apparels_reviews.js
@@ -1,14 +1,38 @@
 const mongoose = require('../db/connections')
 
 const apparelsReviewsSchema = new mongoose.Schema({
-	overallRating: Number,
-	size: String,
+	overallRating: {
+		type: Number,
+		required: true
+	},
+	size: {
+		type: String,
+		required: true
+	},
+    title: {
+		type: String,
+		required: true
+	},
+	description: {
+		type: String,
+		required: false
+	},
 	comfort: String,
 	durability: String,
-    title: String,
-	// apparels: {
-	// 	type: mongoose.Schema.Types.ObjectId,ref:'Apparels'
-	// },
+	timeOfReview: { 
+		type: Date, 
+		default: Date.now 
+	},
+	apparels: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Apparels',
+		required: true
+	},
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Users',
+		require: true
+	}
 })
 
 module.exports = new mongoose.model('ApparelsReviews', apparelsReviewsSchema)
