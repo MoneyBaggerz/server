d795f049e9e0803dccd607a64687ff039c7e17dd
Nevsimao <coday.ink@gmail.com>
2021-05-19 22:09:10 -0400

    updated the router

--- server/controllers/bands.js
+++ server/controllers/bands.js
@@ -3,35 +3,35 @@ const Brands = require('../models/brands')
 const router = express.Router()
 
 // Get brands
-router.get('/', (req, res, next) => {
+router.get('/brands', (req, res, next) => {
 	Brands.find({})
 		.then((brands) => res.json(brands))
 		.catch(next)
 })
 
 // GET a brand
-router.get('/:id', (req, res, next) => {
+router.get('/brands/:id', (req, res, next) => {
 	Brands.findById({ _id: req.params.id })
 		.then((brands) => res.json(brands))
 		.catch(next)
 })
 
 // POST a brand
-router.post('/', (req, res, next) => {
+router.post('/brands', (req, res, next) => {
 	Brands.create(req.body)
 		.then((brands) => res.json(brands))
 		.catch(next)
 })
 
 // PUT a brand
-router.put('/:id', (req, res, next) => {
+router.put('/brands/:id', (req, res, next) => {
 	Brands.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
 		.then((brands) => res.json(brands))
 		.catch(next)
 })
 
 // DELETE a brand
-router.delete('/:id', (req, res, next) => {
+router.delete('/brands/:id', (req, res, next) => {
 	Brands.findByIdAndDelete(req.params.id)
 		.then((brands) => res.json(brands))
 		.catch(next)
