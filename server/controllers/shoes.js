41cd0885bc04b21de20cb7f5aedc6163fda0f622
Nevsimao <coday.ink@gmail.com>
2021-05-19 21:39:18 -0400

    updated the routes

--- server/controllers/shoes.js
+++ server/controllers/shoes.js
@@ -3,35 +3,44 @@ const Shoes = require('../models/shoes')
 const router = express.Router()
 
 // Get shoes
-router.get('/', (req, res, next) => {
+router.get('/shoes', (req, res, next) => {
 	Shoes.find({})
 		.then((shoes) => res.json(shoes))
 		.catch(next)
 })
 
+// Get user shoes
+router.get('/users/shoes', requireToken, (req, res, next) => {
+	Shoes.find({ 
+		user: mongoose.Types.ObjectId(req.user._id)
+	})
+	.then((shoes) => res.json(shoes))
+	.catch(next)
+})
+
 // GET a shoe
-router.get('/:id', (req, res, next) => {
+router.get('/shoes/:id', (req, res, next) => {
 	Shoes.findById({ _id: req.params.id })
 		.then((shoes) => res.json(shoes))
 		.catch(next)
 })
 
 // POST a shoe
-router.post('/', (req, res, next) => {
+router.post('/shoes', requireToken, (req, res, next) => {
 	Shoes.create(req.body)
 		.then((shoes) => res.json(shoes))
 		.catch(next)
 })
 
 // PUT a shoe
-router.put('/:id', (req, res, next) => {
+router.put('/shoes/:id', (req, res, next) => {
 	Shoes.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
 		.then((shoes) => res.json(shoes))
 		.catch(next)
 })
 
 // DELETE a shoe
-router.delete('/:id', (req, res, next) => {
+router.delete('shoes/:id', (req, res, next) => {
 	Shoes.findByIdAndDelete(req.params.id)
 		.then((shoes) => res.json(shoes))
 		.catch(next)
