e3e105da276dc58add593e23562be66a91440792
Nevsimao <coday.ink@gmail.com>
2021-05-19 22:08:54 -0400

    updated the router

--- server/controllers/apparels.js
+++ server/controllers/apparels.js
@@ -1,37 +1,47 @@
 const express = require('express')
 const Apparels = require('../models/apparels')
 const router = express.Router()
+const { requireToken } = require('../middleware/auth')
 
 // Get apparels
-router.get('/', (req, res, next) => {
+router.get('/apparels', (req, res, next) => {
 	Apparels.find({})
 		.then((apparels) => res.json(apparels))
 		.catch(next)
 })
 
+// Get user apparels
+router.get('/users/apparels', requireToken, (req, res, next) => {
+	Apparels.find({ 
+		user: mongoose.Types.ObjectId(req.user._id)
+	})
+	.then((apparels) => res.json(apparels))
+	.catch(next)
+})
+
 // GET a apparel
-router.get('/:id', (req, res, next) => {
+router.get('/apparels/:id', (req, res, next) => {
 	Apparels.findById({ _id: req.params.id })
 		.then((apparels) => res.json(apparels))
 		.catch(next)
 })
 
 // POST a apparel
-router.post('/', (req, res, next) => {
+router.post('/apparels', (req, res, next) => {
 	Apparels.create(req.body)
 		.then((apparels) => res.json(apparels))
 		.catch(next)
 })
 
 // PUT a apparel
-router.put('/:id', (req, res, next) => {
+router.put('/apparels/:id', (req, res, next) => {
 	Apparels.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
 		.then((apparels) => res.json(apparels))
 		.catch(next)
 })
 
 // DELETE a apparel
-router.delete('/:id', (req, res, next) => {
+router.delete('/apparels/:id', (req, res, next) => {
 	Apparels.findByIdAndDelete(req.params.id)
 		.then((apparels) => res.json(apparels))
 		.catch(next)
