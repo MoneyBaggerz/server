ab5bc149b58dd05cb12601deffa7ed00b53ec015
Nevsimao <coday.ink@gmail.com>
2021-05-20 23:52:42 -0400

    had to comment out reviews array conflicting with server for now

--- server/models/shoes.js
+++ server/models/shoes.js
@@ -1,7 +1,9 @@
 const mongoose = require('../db/connections')
 const reviews = require('./shoes_reviews')
+const nanoid = require('nanoid')
 
 const shoesSchema = new mongoose.Schema({
+	_id: Number,
 	name: String,
 	price: Number,
 	size: [Number],
@@ -10,16 +12,16 @@ const shoesSchema = new mongoose.Schema({
 		type: Boolean,
 		required: true
 	},
-	reviews: [reviews],
+	// reviews: [reviews],
 	image: {
 		data: Buffer,
 		contentType: String
 	},
-	brand: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref:'Brands',
-		required: true
-	}
+	// brand: {
+	// 	type: mongoose.Schema.Types.ObjectId,
+	// 	ref:'Brands',
+	// 	required: true
+	// }
 })
 
 module.exports = new mongoose.model('Shoes', shoesSchema)
