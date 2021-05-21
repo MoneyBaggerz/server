d2377dd20fad456154e7065b7afac39849120842
Nevsimao <coday.ink@gmail.com>
2021-05-21 01:06:29 -0400

    working things out with the relations

--- server/models/shoes_reviews.js
+++ server/models/shoes_reviews.js
@@ -12,30 +12,30 @@ const shoeReviewsSchema = new mongoose.Schema({
 		enun: ['small', 'medium', 'large'],
 		required: true
 	},
-    title: {
-		type: String,
-		required: true
-	},
-	description: {
-		type: String,
-		required: false
-	},
-	comfort: String,
-	durability: String,
+    // title: {
+	// 	type: String,
+	// 	required: true
+	// },
+	// description: {
+	// 	type: String,
+	// 	required: false
+	// },
+	// comfort: String,
+	// durability: String,
 	timeOfReview: { 
 		type: Date, 
 		default: Date.now 
 	},
-	shoes: {
-		type: mongoose.Schema.Types.ObjectId, 
-		ref: 'Shoes', 
-		required: true
-	},
-	user: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'Users',
-		required: true
-	}
+	// shoes: {
+	// 	type: mongoose.Schema.Types.ObjectId, 
+	// 	ref: 'Shoes', 
+	// 	required: true
+	// },
+	// user: {
+	// 	type: mongoose.Schema.Types.ObjectId,
+	// 	ref: 'Users',
+	// 	required: true
+	// }
 })
 
 module.exports = new mongoose.model('ShoeReviews', shoeReviewsSchema)
