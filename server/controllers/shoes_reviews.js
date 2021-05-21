59310683d9c0984d36f672904090656b386ba123
Nevsimao <coday.ink@gmail.com>
2021-05-20 23:45:42 -0400

    had to comment some routers due to missing token

--- server/controllers/shoes_reviews.js
+++ server/controllers/shoes_reviews.js
@@ -12,81 +12,81 @@ router.get('/:shoesId/reviews', (req, res, next) => {
 })
 
 // GET current user reviews
-router.get('/reviews', requireToken, (req, res, next)=> {
-	ShoesReviews.find({ author: req.user.id })
-		.then(reviews => res.json(reviews))
-        .catch(next)
-})
+// router.get('/reviews', requireToken, (req, res, next)=> {
+// 	ShoesReviews.find({ author: req.user.id })
+// 		.then(reviews => res.json(reviews))
+//         .catch(next)
+// })
 
 // POST a apparel review
-router.post('/:shoesId/reviews/create', requireToken, (req, res, next) => {
-    const newReviews = {
-        ...req.body,
-        user: req.user._id
-    }
-	ShoesReviews.create(newReviews)
-		.then((newReviews) => {
-            return ShoesReviews.findById(newReviews._id)
-				.populate('user', 'username')
-        })
-        .then((newReviews) => {
-            ShoesReviews.findById({ _id: req.params.shoesId })
-                .then((shoes) => {
-                    shoes.reviews.push(newReviews)
-                    return shoes.save()
-                })
-                .then((shoes) => {
-                    res.json(shoes)
-                })
-        })
-		.catch(next)
-})
+// router.post('/:shoesId/reviews/create', requireToken, (req, res, next) => {
+//     const newReviews = {
+//         ...req.body,
+//         user: req.user._id
+//     }
+// 	ShoesReviews.create(newReviews)
+// 		.then((newReviews) => {
+//             return ShoesReviews.findById(newReviews._id)
+// 				.populate('user', 'username')
+//         })
+//         .then((newReviews) => {
+//             ShoesReviews.findById({ _id: req.params.shoesId })
+//                 .then((shoes) => {
+//                     shoes.reviews.push(newReviews)
+//                     return shoes.save()
+//                 })
+//                 .then((shoes) => {
+//                     res.json(shoes)
+//                 })
+//         })
+// 		.catch(next)
+// })
 
 // PUT a apparel review
-router.put('/:shoesId/reviews/:reviewsId/update', requireToken, (req, res, next) => {
-	ShoesReviews.findById(req.params.shoesId)
-        .then((reviews) => {
-            reviews.post = req.body.post
-            return reviews.save() 
-        })
-        .then((reviews) => {
-            Shoes.findById({ _id: req.params.reviewsId })
-                .then((shoes) => {
-                    shoes.reviews.forEach(
-                        async (aReviews) => {
-                            if (aReviews._id.toString() == reviews._id.toString()) {
-                                aReviews.post = reviews.post
-                                await shoes.save()
-                            }
-                        }
-                    )
-                    res.status(202)
-                })
-                .catch(next)
-        })
-})
+// router.put('/:shoesId/reviews/:reviewsId/update', requireToken, (req, res, next) => {
+// 	ShoesReviews.findById(req.params.shoesId)
+//         .then((reviews) => {
+//             reviews.post = req.body.post
+//             return reviews.save() 
+//         })
+//         .then((reviews) => {
+//             Shoes.findById({ _id: req.params.reviewsId })
+//                 .then((shoes) => {
+//                     shoes.reviews.forEach(
+//                         async (aReviews) => {
+//                             if (aReviews._id.toString() == reviews._id.toString()) {
+//                                 aReviews.post = reviews.post
+//                                 await shoes.save()
+//                             }
+//                         }
+//                     )
+//                     res.status(202)
+//                 })
+//                 .catch(next)
+//         })
+// })
 
 // DELETE a apparel review
-router.delete('/:shoesId/reviews/:reviewsId/delete', requireToken, (req, res, next) => {
-	ShoesReviews.findById(req.params.reviewsId)
-		.then(() => {
-            ShoesReviews.findByIdAndDelete(req.params.reviewsId)
-                .then((reviews) => {
-                    Shoes.findById(req.params.reviewsId)
-                        .then((shoes) => {
-                            shoes.reviews.forEach(
-                                async (aReviews, index) => {
-                                    if (aReviews._id.toString() == reviews._id.toString()) {
-                                        shoes.reviews.splice(index, 1)
-                                        await shoes.save()
-                                    }
-                                }
-                            )
-                        })
-                })
-        })
-        .then(() => res.status(201))
-        .catch(next)
-})
+// router.delete('/:shoesId/reviews/:reviewsId/delete', requireToken, (req, res, next) => {
+// 	ShoesReviews.findById(req.params.reviewsId)
+// 		.then(() => {
+//             ShoesReviews.findByIdAndDelete(req.params.reviewsId)
+//                 .then((reviews) => {
+//                     Shoes.findById(req.params.reviewsId)
+//                         .then((shoes) => {
+//                             shoes.reviews.forEach(
+//                                 async (aReviews, index) => {
+//                                     if (aReviews._id.toString() == reviews._id.toString()) {
+//                                         shoes.reviews.splice(index, 1)
+//                                         await shoes.save()
+//                                     }
+//                                 }
+//                             )
+//                         })
+//                 })
+//         })
+//         .then(() => res.status(201))
+//         .catch(next)
+// })
 
 module.exports = router
