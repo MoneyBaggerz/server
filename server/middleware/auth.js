1e5c4ca501a4f67a47600b31ce1430c942955841
Nevsimao <coday.ink@gmail.com>
2021-05-24 13:35:51 -0400

    reviewd

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,32 +1,32 @@
 // Require the needed npm packages
-const passport = require('passport');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const passport = require('passport')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const secret =
-	process.env.JWT_SECRET || 'some string value only your app knows';
+	process.env.JWT_SECRET || 'some string value only your app knows'
 
-const { Strategy, ExtractJwt } = require('passport-jwt');
+const { Strategy, ExtractJwt } = require('passport-jwt')
 
 const opts = {
 	jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 	secretOrKey: secret,
-};
+}
 
 // Require the user model
-const User = require('../models/Users');
+const User = require('../models/Users')
 
 const strategy = new Strategy(opts, function (jwt_payload, done) {
 	User.findById(jwt_payload.id)
 		.then((user) => done(null, user))
-		.catch((err) => done(err));
-});
+		.catch((err) => done(err))
+})
 
-passport.use(strategy);
+passport.use(strategy)
 
-passport.initialize();
+passport.initialize()
 
-const requireToken = passport.authenticate('jwt', { session: false });
+const requireToken = passport.authenticate('jwt', { session: false })
 
 const createUserToken = (req, user) => {
 	if (
@@ -34,14 +34,14 @@ const createUserToken = (req, user) => {
 		!req.body.password ||
 		!bcrypt.compareSync(req.body.password, user.password)
 	) {
-		const err = new Error('The provided username or password is incorrect');
+		const err = new Error('The provided username or password is incorrect')
 		err.statusCode = 422;
 		throw err;
 	}
-	return jwt.sign({ id: user._id }, secret, { expiresIn: 36000 });
-};
+	return jwt.sign({ id: user._id }, secret, { expiresIn: 36000 })
+}
 
 module.exports = {
 	requireToken,
 	createUserToken,
-};
+}
