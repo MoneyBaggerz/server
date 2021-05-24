d4dc1023120ef8a9164e68e06341b1b6a44e3b96
Nevsimao <coday.ink@gmail.com>
2021-05-24 13:03:45 -0400

    fixed the relations

--- server/models/brands.js
+++ server/models/brands.js
@@ -1,6 +1,6 @@
 const mongoose = require('../db/connections')
-const shoes = require('./shoes')
-const apparels = require('./apparels')
+const shoes = require('./shoes').shoesSchema
+const apparels = require('./apparels').apparelsSchema
 
 const brandsSchema = new mongoose.Schema({
 	name: {
@@ -8,8 +8,10 @@ const brandsSchema = new mongoose.Schema({
         unique: true,
         required: true
     },
-    // shoes: [shoes],
-    // apparels: [apparels]
+    shoes: [shoes],
+    apparels: [apparels]
 })
 
-module.exports = new mongoose.model('Brands', brandsSchema)
+const Brands = mongoose.model('Brands', brandsSchema);
+
+module.exports = {Brands, brandsSchema}
